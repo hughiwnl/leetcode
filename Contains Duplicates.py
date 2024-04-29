97882d51f278804c87bda2bbbd02874db700f8e1
Hugh <imaingodyrlol@gmail.com>
2024-04-28 20:57:58 -0700

    valid anagrams

---
+++ Contains Duplicates.py
@@ -0,0 +1,29 @@
+# my solution:
+# Uses two pointers to iterate through array but it's really slow. 
+class Solution(object):
+    def containsDuplicate(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: bool
+        """
+        n=len(nums)
+        for i in range(n-1):
+            for j in range(i+1,n):
+                if nums[i]==nums[j]:
+                    return True
+            return False
+        
+#optimal solution using hashsets
+#To make the runtime O(N) we can use hashsets which stores unique values
+class Solution(object):
+    def containsDuplicate(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: bool
+        """
+        count = set()
+        for n in nums:
+            if n in count:
+                return True
+            count.add(n)
+        return False
---
+++ Valid Anagram.py
@@ -0,0 +1,13 @@
+#my solution:
+#uses sort function because it's an anagram and if the letters match up, then its true
+class Solution(object):
+    def isAnagram(self, s, t):
+        """
+        :type s: str
+        :type t: str
+        :rtype: bool
+        """
+        sortS = sorted(s)
+        sortT = sorted(t)
+        return sortS == sortT
+
