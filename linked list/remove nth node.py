6c92ba9bfe62ae85bc5f31a35c39d8115b5a5bc2
Hugh <155223694+hughiwnl@users.noreply.github.com>
2025-09-24 11:23:58 -0700

    remove nth node

---
+++ linked list/remove nth node.py
@@ -0,0 +1,21 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def removeNthFromEnd(self, head: Optional[ListNode], n: int) -> Optional[ListNode]:
+        nodes = []
+        cur = head
+        while cur:
+            nodes.append(cur)
+            cur = cur.next
+
+        removeIndex = len(nodes) - n
+        if removeIndex == 0:
+            return head.next # edge case for ex [1,2,3] n = 3 and it removes head
+
+        nodes[removeIndex - 1].next = nodes[removeIndex].next
+
+        return head
+        
