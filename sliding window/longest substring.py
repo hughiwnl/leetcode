38a77343509d8e933ca6afdbcab1f6bb147b1672
Hugh <155223694+hughiwnl@users.noreply.github.com>
2025-10-10 15:19:45 -0700

    longest substring

--- sliding window/longest substring.py
+++ sliding window/longest substring.py
@@ -1,12 +1,13 @@
 class Solution:
     def lengthOfLongestSubstring(self, s: str) -> int:
-        mp = {}
+        charSet = set()
         l = 0
         res = 0
 
         for r in range(len(s)):
-            if s[r] in mp:
-                l = max(mp[s[r]] + 1, l)
-            mp[s[r]] = r
+            while s[r] in charSet:
+                charSet.remove(s[l])
+                l+=1
+            charSet.add(s[r])
             res = max(res, r - l + 1)
         return res
