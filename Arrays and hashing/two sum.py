34bdda73a39a202e567d35ff1774fb2430b8c1c0
Hugh <155223694+hughiwnl@users.noreply.github.com>
2025-10-22 16:53:07 -0700

    binary tree right side

--- Arrays and hashing/two sum.py
+++ Arrays and hashing/two sum.py
@@ -6,3 +6,15 @@ class Solution(object):
                 if nums[i] + nums[j] == target:
                     return [i,j]
         return []
+    
+'''
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        prevMap = {}  # val -> index
+
+        for i, n in enumerate(nums):
+            diff = target - n
+            if diff in prevMap:
+                return [prevMap[diff], i]
+            prevMap[n] = i
+'''
