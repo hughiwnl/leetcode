f2cd3e907d76e6431a6b8442ea47e2396ce464d8
Hugh <155223694+hughiwnl@users.noreply.github.com>
2025-10-24 13:20:28 -0700

    subset notes

--- backtrack/subset.py
+++ backtrack/subset.py
@@ -4,12 +4,12 @@ class Solution:
         subset = []
 
         def dfs(i):
-            if i >= len(nums):
-                res.append(subset.copy())
-                return
-            subset.append(nums[i])
-            dfs(i + 1)
-            subset.pop()
-            dfs(i + 1)
+            if i >= len(nums): #in each branch, once it hits 3 it will append
+                res.append(subset.copy()) # appends to res, basically “Take a photo of what’s in my backpack right now before I change it.”
+                return # goes back a level once its max
+            subset.append(nums[i]) # add thenumber to subset first
+            dfs(i + 1) # goes up one track
+            subset.pop() # removes that 
+            dfs(i + 1) # goes back one and down the track where that number isnt there
         dfs(0)
         return res
