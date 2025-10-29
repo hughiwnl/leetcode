f34970f0a9dd4d2576d3d72e26c258aa8497de94
Hugh <155223694+hughiwnl@users.noreply.github.com>
2025-10-29 09:05:37 -0700

    combination sum

--- backtrack/subset.py
+++ backtrack/subset.py
@@ -13,3 +13,26 @@ class Solution:
             dfs(i + 1) # goes back one and down the track where that number isnt there
         dfs(0)
         return res
+    
+
+#GENERATE PARENTHESIS EXAMPLE
+class Solution:
+    def generateParenthesis(self, n: int) -> List[str]:
+        res = []
+        stack = []
+
+        def backTrack(openN, closedN):
+            if openN == closedN == n:
+                res.append("".join(stack)) # joins it to the stack and removes ""
+                return
+            
+            if openN < n:
+                stack.append("(")
+                backTrack( openN + 1, closedN ) # recursively calls it for different paths
+                stack.pop() #pops the recently added bracket to explore different paths
+            if closedN < openN:
+                stack.append(")")
+                backTrack( openN, closedN + 1)
+                stack.pop()
+        backTrack(0,0)
+        return res
