e96e4c3c5d1f2b45b726e06b83ead0bbef0a90a7
Hugh <155223694+hughiwnl@users.noreply.github.com>
2026-01-12 11:58:00 -0800

    happy number

--- math and geometry/happy number.py
+++ math and geometry/happy number.py
@@ -1,23 +1,23 @@
 class Solution:
     def isHappy(self, n: int) -> bool:
-        slow, fast = n, self.sumOfSquares(n)
-        power = lam = 1
+       visit = set()
 
-        while slow != fast:
-            if power == lam:
-                slow = fast
-                power *= 2
-                lam = 0
-            fast = self.sumOfSquares(fast)
-            lam += 1
-        return True if fast == 1 else False
+        while n not in visit:
+            visit.add(n)
+            n = self.sumOfSquares(n)
 
-    def sumOfSquares(self, n: int) -> int:
+        if n == 1:
+            return True
+
+        return False
+    
+    def sumOfSquares(self, n:int) -> int:
         output = 0
-
+        
         while n:
-            digit = n % 10
-            digit = digit ** 2
-            output += digit
-            n = n // 10
+            num = n % 10
+            output += num**2
+            n = n//10
         return output
+
+
